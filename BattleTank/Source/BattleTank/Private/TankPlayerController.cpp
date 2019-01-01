5f6c15ec877d9feaa2b8e1db33cbef2d95ff885e
sabtain_sarwar <sabtain964@gmail.com>
2019-01-01 09:33:11 +0500

    BT117  Using virtual and override

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -2,7 +2,22 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	// We want to start always by going Super::BeginPlay().Now, what that does is ,it makes sure that the BeginPlay() on the
+	// Super:: classes is being called.(Called the default behavior before we do anything)
+	Super::BeginPlay();
 
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing the tank"));
+	} 
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController  possessing : %s") , *(ControlledTank->GetName()));
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -18,5 +18,15 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	
 public:
 	ATank* GetControlledTank() const;
+
+	// If we want to write (void BeginPlay()) we can, but we want to make sure we are overriding and adding to the functionality
+	// that's further up the inheritance tree, because we want to make sure that all that other stuff that Unreal has written 
+	// is also happening.So,the way you do that is using the keyword override.override says,please make sure there is something
+	// else up the Inheritance tree with exactly the signature (BeginPlay()), precisely BeginPlay() with no parameters.That's 
+	// what the override does,it checks that it can find this upward in the hierarchy. The keyword virtual says my Childrens are 
+	// allowed to override or extend the functionality of this method.So,if the method was declared virtual,in the first place,
+	// which this was in the case of BeginPlay() , it can be overridden by any ancestor in the future.Specifying virtual on an
+	// already virtual method has no extra effect.
+	virtual void BeginPlay() override;
 	
 };
