768e6105cac29b7ded350e7671e1a95852f121a4
sabtain_sarwar <sabtain964@gmail.com>
2019-01-17 16:44:11 +0500

    BT126  Create Default Sub Objects in C

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -9,6 +9,11 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// we're adding the component in the constructor of the tank,because the tank is going to require the TankAimingComponent,
+	// if it's going to delegate aiming to it.
+
+	// No need to protect pointers as added at constructor
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
 }
 
 // Called when the game starts or when spawned
@@ -34,6 +39,7 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 void ATank::AimAt(FVector HitLocation)
 {
-	auto OurTankName = GetName(); // This is the current name of the current tank
-	UE_LOG(LogTemp, Warning, TEXT("%s aimimg at %s"), *OurTankName , *HitLocation.ToString());
+	TankAimingComponent->AimAt(HitLocation);
+	//auto OurTankName = GetName(); // This is the current name of the current tank
+	//UE_LOG(LogTemp, Warning, TEXT("%s aimimg at %s"), *OurTankName , *HitLocation.ToString());
 }
--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -2,9 +2,10 @@
 
 #pragma once
 
+#include "TankAimingComponent.h" // now the tank has a concept of this TankAimingComponent
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
-#include "Tank.generated.h"
+#include "Tank.generated.h" // Put New includes above
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -17,12 +18,20 @@ public:
 	// thing that oyr LineTrace hit.
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	// we are putting it in the protected section.Why protected? Well it can't be private,because we're going to need to access 
+	// it using a UPROPPERTY later,and it's not going to be allowed to be private we'll get a compiler error.And it doesn't 
+	// need to be public because who needs access to this component from the outside.
+
+	// we're going to make ourself a TankMovementComponent pointer.The TankMovementComponent exist as a class  
+	UTankAimingComponent* TankAimingComponent = nullptr; // nullptr just to initialize it to something sensible and predictable.
+	// The tank now has the ability to have a refrence to a TankAimingComponent
 
 private:	
 	// Sets default values for this pawn's properties
 	ATank();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
