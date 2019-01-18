dad49ee7a6cf45ca3a8f71d2b09eecb039ae7a53
sabtain_sarwar <sabtain964@gmail.com>
2019-01-18 12:06:36 +0500

    BT129  Predict Projectile Landing Point

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -42,6 +42,27 @@ void UTankAimingComponent::AimAt(FVector HitLocation , float LaunchSpeed)
 	//auto OurTankName = GetOwner()->GetName(); 
 	//auto BarrelLocation = Barrel->GetComponentLocation().ToString(); // a refrence to barrel and getting its location
 	//UE_LOG(LogTemp, Warning, TEXT("%s aimimg at %s from %s"), *OurTankName, *HitLocation.ToString() , *BarrelLocation);
-	
-	UE_LOG(LogTemp, Warning, TEXT("Firing at %f"), LaunchSpeed);
+	//UE_LOG(LogTemp, Warning, TEXT("Firing at %f"), LaunchSpeed);
+	if (!Barrel) { return; }
+	FVector OutLaunchVelocity;
+	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
+	if (UGameplayStatics::SuggestProjectileVelocity // calculate the outLaunchVelocity
+			(
+				this, // refrence to actual TankAimingComponent
+				OutLaunchVelocity, // that is an OUT parameter
+				StartLocation,
+				HitLocation,
+				LaunchSpeed,
+				false, // we dont't want to go by the HighArc so we say false
+				0, // CollisionRadius.How much variablity do we want on it ? 0 bcz we want to aim precisely
+				0, // OverrideGravityZ. No we're not going to do 
+				ESuggestProjVelocityTraceOption::DoNotTrace // is that you need to decide whether we're going to trace or not
+				// whether we're going to do debug trace
+			)
+		)
+	{
+		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
+		UE_LOG(LogTemp, Warning, TEXT("Aiming at %s"), *AimDirection.ToString());
+	}
+	// if no solution found do nothing
 }
--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "Engine.h"
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
